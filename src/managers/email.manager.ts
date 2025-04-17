bda0af67556e97be8bef7b0e9bbab2878e2d2bc1
Bogdan-design <bogdanbw@gmail.com>
2025-04-17 17:46:48 +0200

    move to classes

--- src/managers/email.manager.ts
+++ src/managers/email.manager.ts
@@ -7,7 +7,7 @@ export const emailsManager = {
         await emailAdapter.sendEmail({
             email:newUserFromDB.accountData.email,
             message: `<h1>Thanks for your registration</h1>
-        <p>To finish registration please follow the link below:
+        <p>To finish registration please, follow the link below:
             <a href='https://somesite.com/confirm-email?code=${newUserFromDB.emailConfirmation.confirmationCode}'>complete registration</a>
         </p>`,
             subject: 'Please confirm your email'
